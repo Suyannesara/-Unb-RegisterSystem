d0d65432cf22e60456ee422ab9b46bbcdafd3f31
Suyanne Miranda <66568437+Suyannesara@users.noreply.github.com>
2023-01-12 21:47:30 -0300

    ♻️refact: change strategy to ask for attributes one by one

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 
 int main()
 {
-    char *personAttributes[6] = {"cpf", "nome", "sexo", "data de nascimento", "cidade", "UF"};
-    int i;
-    int alreadyRegistered = 0;
+    // char *personAttributes[6] = {"cpf", "nome", "sexo", "data de nascimento", "cidade", "UF"};
+    // int i;
+    // int alreadyRegistered = 0;
 
     typedef struct 
     {
@@ -22,46 +22,73 @@ int main()
     Person pr;
     // Limpar buffer
 
-    for (i = 0; i < 6; i++)
+
+    printf("Digite o CPF da pessoa: \n");
+    scanf("%[^\n]", &pw.Cpf);
+
+    printf("Digite o nome da pessoa: \n");
+    scanf("%[^\n]", &pw.Name);
+
+    printf("Digite o Sexo da pessoa: \n");
+    scanf("%[^\n]", &pw.Sex);
+
+    printf("Digite a data de nascimento da pessoa: \n");
+    scanf("%[^\n]", &pw.DtBorn);
+
+    printf("Digite a cidade da pessoa: \n");
+    scanf("%[^\n]", &pw.City);
+
+    printf("Digite a UF/Estado da pessoa: \n");
+    scanf("%[^\n]", &pw.Uf);
+
+   
+    //Open file for writing
+    fp = fopen("person.txt", "a");
+
+    if (fp != NULL)
     {
-        printf("Digite o %s da pessoa: \n", personAttributes[i]);
-        // "%[^\n]" - Read all until user press enter
-        scanf("%[^\n]", &pw.personAttributes[i]);
+        //print ON File
+        fprintf(fp, "%s\n", pw.Cpf);
+        fprintf(fp, "%s\n", pw.Name);
+        fprintf(fp, "%s\n", pw.Sex);
+        fprintf(fp, "%s\n", pw.DtBorn);
+        fprintf(fp, "%s\n", pw.City);
+        fprintf(fp, "%s\n", pw.Uf);
 
-        // //Open file for writing
-        // fp = fopen("person.txt", "a");
+        printf("Pessoa cadastrada com sucesso! \n");
 
-        // //printFile
-        // fprintf(fp, "%s\n", pw.Cpf);
-        // fclose(fp);
+        // Close file
+        fclose(fp);
     }
+    
 
     // vERIFY IF THERES A PERSON WITH A REGISTER CPF ALREADY ON FILE
     //Open file for reading
     fp = fopen("person.txt", "r");
 
-    for (i = 0; i < 6; i++)
+    if (!feof(fp))
     {
-        if (!feof(fp))
-        {
-            return;
-        }
-
-        fscanf(fp, "%^\n", pr.Cpf);
-
-        if (strcmp(pw.Cpf, pr.Cpf) != 0)
-        {
-            fp = fopen("person.txt", "a");
-            printf("Pessoa cadastrada com sucesso");
-            // alreadyRegistered += 1;
-        }else {
-            printf("Nao eh possivel cadastrar, ja tem esse cpf");
-        }
-
+        return;
+    }else {
+        // Insert data in PR variable
+        fscanf(fp, "%^\n", &pr.Cpf);
+        fscanf(fp, "%^\n", &pr.Name);
+        fscanf(fp, "%^\n", &pr.Sex);
+        fscanf(fp, "%^\n", pr.DtBorn);
+        fscanf(fp, "%^\n", &pr.City);
+        fscanf(fp, "%^\n", &pr.Uf);
     }
 
-    
-    /* code */
+
+    if (strcmp(pw.Cpf, pr.Cpf) != 0)
+    {
+        fp = fopen("person.txt", "a");
+        printf("Pessoa cadastrada com sucesso 22\n");
+        // alreadyRegistered += 1;
+    }else {
+        printf("Nao eh possivel cadastrar, ja tem esse cpf");
+    }
+
     return 0;
 }
 
